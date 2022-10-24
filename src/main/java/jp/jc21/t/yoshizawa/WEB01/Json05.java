6025b5764c0f33a4fdb5e746f6ec7a479964befe
200754 <200754@jc-21.jp>
2022-10-24 14:24:21 +0900

    Sentiment.javaが動いた

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
@@ -21,9 +21,10 @@ public class Json05 {
 	static Language getLanguage(String s) throws IOException, URISyntaxException, InterruptedException {
 		Gson gson = new Gson();
 
-		String url = "https://r04jk3ab-text.cognitiveservices.azure.com//" + "text/analytics/v3.0/languages";
+		String url = "https://r04jk3b12-text.cognitiveservices.azure.com/"
+				+"text/analytics/v3.0/languages";
 		Map<String, String> map = new HashMap<>();
-		map.put("Ocp-Apim-Subscription-Key", "953ea066e1384085b69e5bd5cb3c20d0");
+		map.put("Ocp-Apim-Subscription-Key", "75619835205245c88a0db12a2a6e8220");
 
 		Docs doc = new Docs();
 		doc.id = "1";
@@ -60,6 +61,7 @@ class Documents {
 
 class DetectedLanguage {
 	String name;
+	String iso6391Name;
 }
 
 class Source {
---
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
@@ -0,0 +1,95 @@
+package jp.jc21.t.yoshizawa.WEB01;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class Sentiment {
+
+	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+		Sentiments message = getLanguage("Stepover Toehold With Facelock");
+		if (message != null) {
+			System.out.println("Positive:"+message.documents[0].confidenceScores.positive);
+			System.out.println("Neutral:"+message.documents[0].confidenceScores.neutral);
+			System.out.println("Negative:"+message.documents[0].confidenceScores.negative);
+		}
+	}
+
+	static Sentiments getLanguage(String s) throws IOException, URISyntaxException, InterruptedException {
+		Gson gson = new Gson();
+		String lang="";
+		
+		try {
+			Language result = Json05.getLanguage(s);
+			lang = result.documents[0].detectedLanguage.iso6391Name;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String url = "https://r04jk3b12-text.cognitiveservices.azure.com/"
+				+"text/analytics/v3.0/sentiment";
+		Map<String, String> map = new HashMap<>();
+		map.put("Ocp-Apim-Subscription-Key", "75619835205245c88a0db12a2a6e8220");
+
+		Docs2 doc = new Docs2();
+		doc.language = lang;
+		doc.id = "1";
+		doc.text = s;
+
+		Source2 src = new Source2();
+		src.documents = new Docs2[1];
+		src.documents[0] = doc;
+
+		String jsonData = new Gson().toJson(src);
+
+		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
+
+		JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
+		Sentiments message = null;
+		if (reader != null) {
+			message = gson.fromJson(reader, Sentiments.class);
+			reader.close();
+		}
+		return message;
+	}
+
+}
+
+class Sentiments {
+	Documents2[] documents;
+	String[] errors;
+	String modelVersion;
+}
+
+class Documents2 {
+	String id;
+	String sentiment;
+	ConfidenceScores confidenceScores;
+	
+}
+
+class  ConfidenceScores{
+	float positive;
+	float neutral;
+	float negative;
+}
+
+class Source2 {
+	Docs2[] documents;
+}
+
+class Docs2 {
+	String language;
+	String id;
+	String text;
+}
